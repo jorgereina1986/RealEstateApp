8a0c01d1e6a8c92d040a22da95900c2b40f018ba
Jorge Reina <jorgereina@Jorges-MBP.home>
2017-11-28 23:48:21 -0500

    connected to firebase

--- app/src/main/java/com/jorgereina/www/realestateapp/MainActivity.java
+++ app/src/main/java/com/jorgereina/www/realestateapp/MainActivity.java
@@ -2,12 +2,42 @@ package com.jorgereina.www.realestateapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputEt;
+    private Button okButton;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputEt = findViewById(R.id.input_et);
+        okButton = findViewById(R.id.ok_btn);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        databaseReference.setValue("test2");
+            }
+        });
+
+
+
+
     }
 }
